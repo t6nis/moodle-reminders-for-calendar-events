1028c8f5f0691a64dc5cba64bf3331820b270728
wehr <mariowehr@gmx.at>
2014-07-18 19:39:44 +0200

    Finished 2.7 migrations

--- classes/task/reminder_task.php
+++ classes/task/reminder_task.php
@@ -65,7 +65,244 @@ class reminder_task extends \core\task\scheduled_task {
         // Shown in admin screens
         return get_string('reminderruntask', 'local_reminders');
     }
+    
+    /**
+     * Returns true if input string is empty/whitespaces only, otherwise false.
+     *
+     * @param type $str string
+     *
+     * @return boolean true if string is empty or whitespace
+     */
+    public function isemptyString($str) {
+        return !isset($str) || empty($str) || trim($str) === '';
+    }
 
+    /**
+     * Taken from Moodle API, striped log event generation, we don't want that every send message event goes right into our log :-)
+     *
+     * Returns true if input string is empty/whitespaces only, otherwise false.
+     *
+     * @param type $str string
+     *
+     * @return boolean true if string is empty or whitespace
+     */
+    protected function reminder_message_send($eventdata) {
+        global $CFG, $DB;
+
+        //new message ID to return
+        $messageid = false;
+
+        // Fetch default (site) preferences
+        $defaultpreferences = get_message_output_default_preferences();
+        $preferencebase = $eventdata->component.'_'.$eventdata->name;
+        // If message provider is disabled then don't do any processing.
+        if (!empty($defaultpreferences->{$preferencebase.'_disable'})) {
+            return $messageid;
+        }
+
+        //TODO: we need to solve problems with database transactions here somehow, for now we just prevent transactions - sorry
+        $DB->transactions_forbidden();
+
+        // By default a message is a notification. Only personal/private messages aren't notifications.
+        if (!isset($eventdata->notification)) {
+            $eventdata->notification = 1;
+        }
+
+        if (is_number($eventdata->userto)) {
+            $eventdata->userto = \core_user::get_user($eventdata->userto);
+        }
+        if (is_int($eventdata->userfrom)) {
+            $eventdata->userfrom = \core_user::get_user($eventdata->userfrom);
+        }
+
+        $usertoisrealuser = (\core_user::is_real_user($eventdata->userto->id) != false);
+        // If recipient is internal user (noreply user), and emailstop is set then don't send any msg.
+        if (!$usertoisrealuser && !empty($eventdata->userto->emailstop)) {
+            debugging('Attempt to send msg to internal (noreply) user', DEBUG_NORMAL);
+            return false;
+        }
+
+        if (!isset($eventdata->userto->auth) or !isset($eventdata->userto->suspended) or !isset($eventdata->userto->deleted)) {
+            $eventdata->userto = \core_user::get_user($eventdata->userto->id);
+        }
+
+        //after how long inactive should the user be considered logged off?
+        if (isset($CFG->block_online_users_timetosee)) {
+            $timetoshowusers = $CFG->block_online_users_timetosee * 60;
+        } else {
+            $timetoshowusers = 300;//5 minutes
+        }
+
+        // Work out if the user is logged in or not
+        if (!empty($eventdata->userto->lastaccess) && (time()-$timetoshowusers) < $eventdata->userto->lastaccess) {
+            $userstate = 'loggedin';
+        } else {
+            $userstate = 'loggedoff';
+        }
+
+        // Create the message object
+        $savemessage = new \stdClass();
+        $savemessage->useridfrom        = $eventdata->userfrom->id;
+        $savemessage->useridto          = $eventdata->userto->id;
+        $savemessage->subject           = $eventdata->subject;
+        $savemessage->fullmessage       = $eventdata->fullmessage;
+        $savemessage->fullmessageformat = $eventdata->fullmessageformat;
+        $savemessage->fullmessagehtml   = $eventdata->fullmessagehtml;
+        $savemessage->smallmessage      = $eventdata->smallmessage;
+        $savemessage->notification      = $eventdata->notification;
+
+        if (!empty($eventdata->contexturl)) {
+            $savemessage->contexturl = $eventdata->contexturl;
+        } else {
+            $savemessage->contexturl = null;
+        }
+
+        if (!empty($eventdata->contexturlname)) {
+            $savemessage->contexturlname = $eventdata->contexturlname;
+        } else {
+            $savemessage->contexturlname = null;
+        }
+
+        $savemessage->timecreated = time();
+
+        // Fetch enabled processors
+        $processors = get_message_processors(true);
+
+        // Preset variables
+        $processorlist = array();
+        // Fill in the array of processors to be used based on default and user preferences
+        foreach ($processors as $processor) {
+            // Skip adding processors for internal user, if processor doesn't support sending message to internal user.
+            if (!$usertoisrealuser && !$processor->object->can_send_to_any_users()) {
+                continue;
+            }
+
+            // First find out permissions
+            $defaultpreference = $processor->name.'_provider_'.$preferencebase.'_permitted';
+            if (isset($defaultpreferences->{$defaultpreference})) {
+                $permitted = $defaultpreferences->{$defaultpreference};
+            } else {
+                // MDL-25114 They supplied an $eventdata->component $eventdata->name combination which doesn't
+                // exist in the message_provider table (thus there is no default settings for them).
+                $preferrormsg = "Could not load preference $defaultpreference. Make sure the component and name you supplied
+                    to message_send() are valid.";
+                throw new coding_exception($preferrormsg);
+            }
+
+            // Find out if user has configured this output
+            // Some processors cannot function without settings from the user
+            $userisconfigured = $processor->object->is_user_configured($eventdata->userto);
+
+            // DEBUG: notify if we are forcing unconfigured output
+            if ($permitted == 'forced' && !$userisconfigured) {
+                debugging('Attempt to force message delivery to user who has "'.$processor->name.'" output unconfigured', DEBUG_NORMAL);
+            }
+
+            // Warn developers that necessary data is missing regardless of how the processors are configured
+            if (!isset($eventdata->userto->emailstop)) {
+                debugging('userto->emailstop is not set. Retrieving it from the user table');
+                $eventdata->userto->emailstop = $DB->get_field('user', 'emailstop', array('id'=>$eventdata->userto->id));
+            }
+
+            // Populate the list of processors we will be using
+            if ($permitted == 'forced' && $userisconfigured) {
+                // An admin is forcing users to use this message processor. Use this processor unconditionally.
+                $processorlist[] = $processor->name;
+            } else if ($permitted == 'permitted' && $userisconfigured && !$eventdata->userto->emailstop) {
+                // User has not disabled notifications
+                // See if user set any notification preferences, otherwise use site default ones
+                $userpreferencename = 'message_provider_'.$preferencebase.'_'.$userstate;
+                if ($userpreference = get_user_preferences($userpreferencename, null, $eventdata->userto->id)) {
+                    if (in_array($processor->name, explode(',', $userpreference))) {
+                        $processorlist[] = $processor->name;
+                    }
+                } else if (isset($defaultpreferences->{$userpreferencename})) {
+                    if (in_array($processor->name, explode(',', $defaultpreferences->{$userpreferencename}))) {
+                        $processorlist[] = $processor->name;
+                    }
+                }
+            }
+        }
+
+        if (empty($processorlist) && $savemessage->notification) {
+            //if they have deselected all processors and its a notification mark it read. The user doesnt want to be bothered
+            $savemessage->timeread = time();
+            $messageid = $DB->insert_record('message_read', $savemessage);
+        } else {                        // Process the message
+            // Store unread message just in case we can not send it
+            $messageid = $savemessage->id = $DB->insert_record('message', $savemessage);
+            $eventdata->savedmessageid = $savemessage->id;
+
+            // Try to deliver the message to each processor
+            if (!empty($processorlist)) {
+                foreach ($processorlist as $procname) {
+                    if (!$processors[$procname]->object->send_message($eventdata)) {
+                        debugging('Error calling message processor '.$procname);
+                        $messageid = false;
+                    }
+                }
+
+                //if messaging is disabled and they previously had forum notifications handled by the popup processor
+                //or any processor that puts a row in message_working then the notification will remain forever
+                //unread. To prevent this mark the message read if messaging is disabled
+                if (empty($CFG->messaging)) {
+                    require_once($CFG->dirroot.'/message/lib.php');
+                    $messageid = message_mark_message_read($savemessage, time());
+                } else if ( $DB->count_records('message_working', array('unreadmessageid' => $savemessage->id)) == 0){
+                    //if there is no more processors that want to process this we can move message to message_read
+                    require_once($CFG->dirroot.'/message/lib.php');
+                    $messageid = message_mark_message_read($savemessage, time(), true);
+                }
+            }
+        }
+
+        return $messageid;
+    }
+    /**
+     * Function to retrive module instace from corresponding module
+     * table. This function is written because when sending reminders
+     * it can restrict showing some fields in the message which are sensitive
+     * to user. (Such as some descriptions are hidden until defined date)
+     * Function is very similar to the function in datalib.php/get_coursemodule_from_instance,
+     * but by below it returns all fields of the module.
+     *
+     * Eg: can get the quiz instace from quiz table, can get the new assignment
+     * instace from assign table, etc.
+     *
+     * @param string $modulename name of module type, eg. resource, assignment,...
+     * @param int $instance module instance number (id in resource, assignment etc. table)
+     * @param int $courseid optional course id for extra validation
+     *
+     * @return individual module instance (a quiz, a assignment, etc).
+     *          If fails returns null
+     */
+     public function fetch_module_instance($modulename, $instance, $courseid=0) {
+        global $DB;
+
+        $params = array('instance'=>$instance, 'modulename'=>$modulename);
+
+        $courseselect = "";
+
+        if ($courseid) {
+            $courseselect = "AND cm.course = :courseid";
+            $params['courseid'] = $courseid;
+        }
+
+        $sql = "SELECT m.*
+                  FROM {course_modules} cm
+                       JOIN {modules} md ON md.id = cm.module
+                       JOIN {".$modulename."} m ON m.id = cm.instance
+                 WHERE m.id = :instance AND md.name = :modulename
+                       $courseselect";
+
+        try {
+            return $DB->get_record_sql($sql, $params, IGNORE_MISSING);
+        } catch (moodle_exception $mex) {
+            mtrace('  [Local Reminder - ERROR] Failed to fetch module instance! '.$mex.getMessage);
+            return null;
+        }
+    }
+    
     public function execute() {
         global $CFG, $DB;
 
@@ -166,7 +403,7 @@ class reminder_task extends \core\task\scheduled_task {
 
         // iterating through each event...
         foreach ($upcomingevents as $event) {
-            $event = new calendar_event($event);
+            $event = \calendar_event::create($event, false);
 
             $aheadday = 0;
 
@@ -273,13 +510,13 @@ class reminder_task extends \core\task\scheduled_task {
 
                     case 'due':
 
-                        if (!isemptyString($event->modulename)) {
+                        if (!$this->isemptyString($event->modulename)) {
                             $course = $DB->get_record('course', array('id' => $event->courseid));
                             $cm = get_coursemodule_from_instance($event->modulename, $event->instance, $event->courseid);
 
                             if (!empty($course) && !empty($cm)) {
-                                $activityobj = fetch_module_instance($event->modulename, $event->instance, $event->courseid);
-                                $context = context_module::instance($cm->id);
+                                $activityobj = $this->fetch_module_instance($event->modulename, $event->instance, $event->courseid);
+                                $context = \context_module::instance($cm->id);
 
                                 // patch provided by Julien Boulen (jboulen)
                                 // to prevent a user receives an alert for an activity that he can't see.
@@ -289,7 +526,7 @@ class reminder_task extends \core\task\scheduled_task {
                                 } else {
                                     $sendusers = groups_get_grouping_members($cm->groupingid);
                                 }
-                                $reminder = new due_reminder($event, $course, $context, $aheadday);
+                                $reminder = new \due_reminder($event, $course, $context, $aheadday);
                                 $reminder->set_activity($event->modulename, $activityobj);
                                 $eventdata = $reminder->create_reminder_message_object($fromuser);
                             }
@@ -305,8 +542,8 @@ class reminder_task extends \core\task\scheduled_task {
 
                             // add module details, if this event is a mod type event
                             //
-                            if (!isemptyString($event->modulename) && $event->courseid > 0) {
-                                $activityobj = fetch_module_instance($event->modulename, $event->instance, $event->courseid);
+                            if (!$this->isemptyString($event->modulename) && $event->courseid > 0) {
+                                $activityobj = $this->fetch_module_instance($event->modulename, $event->instance, $event->courseid);
                                 $reminder->set_activity($event->modulename, $activityobj);
                             }
                             $eventdata = $reminder->create_reminder_message_object($fromuser);
@@ -324,17 +561,17 @@ class reminder_task extends \core\task\scheduled_task {
                         break;
 
                     default:
-                        if (!isemptyString($event->modulename)) {
+                        if (!$this->isemptyString($event->modulename)) {
                             $course = $DB->get_record('course', array('id' => $event->courseid));
                             $cm = get_coursemodule_from_instance($event->modulename, $event->instance, $event->courseid);
 
                             if (!empty($course) && !empty($cm)) {
-                                $activityobj = fetch_module_instance($event->modulename, $event->instance, $event->courseid);
-                                $context = context_module::instance($cm->id);
+                                $activityobj = $this->fetch_module_instance($event->modulename, $event->instance, $event->courseid);
+                                $context = \context_module::instance($cm->id);
                                 $sendusers = get_role_users($activityroleids, $context, true, 'u.*');
 
                                 //$sendusers = get_enrolled_users($context, '', $event->groupid, 'u.*');
-                                $reminder = new due_reminder($event, $course, $context, $aheadday);
+                                $reminder = new \due_reminder($event, $course, $context, $aheadday);
                                 $reminder->set_activity($event->modulename, $activityobj);
                                 $eventdata = $reminder->create_reminder_message_object($fromuser);
                             }
@@ -375,10 +612,10 @@ class reminder_task extends \core\task\scheduled_task {
                 //mtrace($eventdata->fullmessagehtml);
                 //mtrace("-----------------------------------");
                 try {
-                    $mailresult = message_send($eventdata);
+                    $mailresult = $this->reminder_message_send($eventdata);
 
                     if (!$mailresult) {
-                        throw new coding_exception("Could not send out message for event#$event->id to user $eventdata->userto");
+                        throw new \coding_exception("Could not send out message for event#$event->id to user $eventdata->userto");
                     }
                 } catch (moodle_exception $mex) {
                     $failedcount++;
@@ -402,61 +639,5 @@ class reminder_task extends \core\task\scheduled_task {
         ));
         $event->trigger();
         //add_to_log(0, 'local_reminders', 'cron', '', $timewindowend, 0, 0);
-    }
-
-    /**
-     * Function to retrive module instace from corresponding module
-     * table. This function is written because when sending reminders
-     * it can restrict showing some fields in the message which are sensitive
-     * to user. (Such as some descriptions are hidden until defined date)
-     * Function is very similar to the function in datalib.php/get_coursemodule_from_instance,
-     * but by below it returns all fields of the module.
-     *
-     * Eg: can get the quiz instace from quiz table, can get the new assignment
-     * instace from assign table, etc.
-     *
-     * @param string $modulename name of module type, eg. resource, assignment,...
-     * @param int $instance module instance number (id in resource, assignment etc. table)
-     * @param int $courseid optional course id for extra validation
-     *
-     * @return individual module instance (a quiz, a assignment, etc).
-     *          If fails returns null
-     */
-    function fetch_module_instance($modulename, $instance, $courseid=0) {
-        global $DB;
-
-        $params = array('instance'=>$instance, 'modulename'=>$modulename);
-
-        $courseselect = "";
-
-        if ($courseid) {
-            $courseselect = "AND cm.course = :courseid";
-            $params['courseid'] = $courseid;
-        }
-
-        $sql = "SELECT m.*
-                  FROM {course_modules} cm
-                       JOIN {modules} md ON md.id = cm.module
-                       JOIN {".$modulename."} m ON m.id = cm.instance
-                 WHERE m.id = :instance AND md.name = :modulename
-                       $courseselect";
-
-        try {
-            return $DB->get_record_sql($sql, $params, IGNORE_MISSING);
-        } catch (moodle_exception $mex) {
-            mtrace('  [Local Reminder - ERROR] Failed to fetch module instance! '.$mex.getMessage);
-            return null;
-        }
-    }
-
-    /**
-     * Returns true if input string is empty/whitespaces only, otherwise false.
-     *
-     * @param type $str string
-     *
-     * @return boolean true if string is empty or whitespace
-     */
-    function isemptyString($str) {
-        return !isset($str) || empty($str) || trim($str) === '';
     }
 }
