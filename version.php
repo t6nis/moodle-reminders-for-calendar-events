8decf101bc4ab51bd1629d2742fee12398c2c5cb
wehr <mariowehr@gmx.at>
2014-08-02 15:37:14 +0200

    Raised version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014073100;
+$plugin->version   = 2014080200;
 $plugin->requires  = 2013051400;  
 $plugin->release   = '2.0.0';
 $plugin->maturity  = MATURITY_RC;
